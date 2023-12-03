e6de8578f3ea317850975eb28e3df6ffa88e54a9
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-03 15:36:18 -0300

    2° commit - tdd model: name consegue ser preenchido? RED

---
+++ spec/models/heroes_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Hero, type: :model do
+
+  before{@hero = Hero.new}
+
+  describe 'Testes de preenchimento do model Hero' do
+
+    it 'name consegue ser preenchido?' do
+      @hero.name = ''
+      expect(@hero.name).to eq('super homem')
+    end
+
+  end
+
+end
